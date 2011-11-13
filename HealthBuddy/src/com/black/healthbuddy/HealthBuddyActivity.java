c329db21937396bc94525f6d1c8b03d9ac827a60
elena <Elena.Magno@ucdconnect.ie>
2011-11-13 15:10:00 +0000

    Framework update: added some activities and stub views. Unfinished.

--- HealthBuddy/src/com/black/healthbuddy/HealthBuddyActivity.java
+++ HealthBuddy/src/com/black/healthbuddy/HealthBuddyActivity.java
@@ -1,16 +1,21 @@
 package com.black.healthbuddy;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class HealthBuddyActivity extends Activity {
-    /** Called when the activity is first created. */
+
+    // Called when the activity is first created. 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the view defined by Main.xml
         setContentView(R.layout.main);
+      
+        
     }
-    
-  // test 3 
-    
+        
 }
+
