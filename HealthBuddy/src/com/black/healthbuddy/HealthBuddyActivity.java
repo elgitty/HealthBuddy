34fdc4fbf4e561a76abfa106fad53cfac62d1191
elena <Elena.Magno@ucdconnect.ie>
2011-11-13 22:03:54 +0000

    Created framework: you can now click on any button to traverse the app.

--- HealthBuddy/src/com/black/healthbuddy/HealthBuddyActivity.java
+++ HealthBuddy/src/com/black/healthbuddy/HealthBuddyActivity.java
@@ -7,13 +7,12 @@ import android.os.Bundle;
 
 public class HealthBuddyActivity extends Activity {
 
-    // Called when the activity is first created. 
+    /** Called when the activity is first created */ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // set the view defined by Main.xml
-        setContentView(R.layout.main);
-      
+        setContentView(R.layout.main);      
         
     }
         
